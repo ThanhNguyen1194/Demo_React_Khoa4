ba8c69d138e4e630797b8e1225bc58b8713ad19e
ThanhNguyen <nguyenhoaithanhlt@gmail.com>
2022-09-11 15:21:59 +0700

    upload

---
+++ .history/src/redux/configStore_20220910204347.js
@@ -0,0 +1,42 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import reduxThunk from "redux-thunk";
+import ToDoListReducer from "./reducers/TodolistReducer";
+import LoadingReducer from "./reducers/LoadingReducer";
+import {ModalReducer} from "./reducers/ModalReducer";
+import {HistoryReducer} from "./reducers/HistoryReducer";
+import {UserLoginCyberBugsReducer} from "./reducers/UserCyberBugsReducer"
+import {ProjectCategoryReducer} from "./reducers/ProjectCategoryReducer"
+import {ProjectCyberBugsReducer} from "./reducers/ProjectCyberBugsReducer"
+import { drawerReducer } from './reducers/DrawerCyberbugs';
+import {TaskTypeReducer} from "./reducers/TaskTypeReducer";
+import {PriorityReducer} from "./reducers/PriorityReducer";
+
+import createMiddleWareSaga from "redux-saga"
+
+import { rootSaga } from "./Sagas/rootSaga";
+import { ProjectReducer } from "./reducers/ProjectReducer";
+import { StatusReducer } from './reducers/StatusReducer';
+
+const middleWareSaga = createMiddleWareSaga();
+
+
+const rootReducer = combineReducers({
+    ToDoListReducer,
+    LoadingReducer,
+    ModalReducer,
+    HistoryReducer,
+    UserLoginCyberBugsReducer,
+    ProjectCategoryReducer,
+    ProjectCyberBugsReducer,
+    drawerReducer,
+    ProjectReducer,
+    TaskTypeReducer,
+    PriorityReducer,
+    StatusReducer
+})
+
+const store = createStore(rootReducer,applyMiddleware(reduxThunk,middleWareSaga));
+
+middleWareSaga.run(rootSaga);
+
+export default store;
---
+++ .history/src/redux/reducers/UserCyberBugsReducer_20220906200210.js
@@ -0,0 +1,37 @@
+import { USLOGIN } from "../constants/Cyberbugs/Cyberbugs";
+
+const { USER_LOGIN } = require("../../util/constants/settingSystem");
+
+let usLogin = {};
+
+if(localStorage.getItem(USER_LOGIN)) {
+    usLogin = JSON.parse(localStorage.getItem(USER_LOGIN));
+}
+
+
+
+
+const stateDefault =  {
+    userLogin : usLogin,
+    userSearch: []
+}
+
+
+
+export const UserLoginCyberBugsReducer = (state = stateDefault,action) => {
+    switch(action.type) {
+        case USLOGIN: {
+            state.userLogin = action.userLogin;
+            return {...state}
+        }
+
+        case 'GET_USER_SEARCH' :{
+            state.userSearch = action.lstUserSearch;
+            // console.log('stateUser',state);
+            return {...state}
+        }
+
+
+        default : return {...state};
+    }
+}
